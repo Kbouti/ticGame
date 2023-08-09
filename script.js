8dfd87bd073be335e17331459dc89edd9157ab86
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-09 14:32:54 -0700

    Relocate script tag to below body

--- script.js
+++ script.js
@@ -17,13 +17,20 @@ const gameboardDiv = document.getElementById(`gameboard`);
 const optionsForm = document.getElementById(`options`)
 const player1Input = document.getElementById(`player1Name`);
 const player2Input = document.getElementById(`player2Name`);
+const submitButton = document.getElementById(`submit`)
 
 
 optionsForm.addEventListener(`submit`, function(){
     event.preventDefault();
     console.log(`shitsnacks`);
+
+    player1Input.textContent = 'did something friggin work??';
+    formReset();
+
 })
 
-function formReset(){
 
+function formReset(){
+player1Input.textContent = ``;
+player2Input.innerHTML = ``;
 }
