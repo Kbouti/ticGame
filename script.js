74f8baf402fdb8d6a4e34a8e56f8361808a23d58
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-09 14:18:54 -0700

    Fix typo in player2Name id

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let cacheDom = (function(){
+// let cacheDom = (function(){
+// const gameboardDiv = document.getElementById(`gameboard`);
+// const optionsForm = document.getElementById(`options`)
+
+
+// optionsForm.addEventListener(`submit`), function(){
+//     event.preventDefault();
+//     console.log(`shitsnacks`);
+
+// }
+// return gameboardDiv, optionsForm;
+// })()
+
+// cacheDom();
+
 const gameboardDiv = document.getElementById(`gameboard`);
+const optionsForm = document.getElementById(`options`)
+const player1Input = document.getElementById(`player1Name`);
+const player2Input = document.getElementById(`player2Name`);
 
-})()
 
+optionsForm.addEventListener(`submit`, function(){
+    event.preventDefault();
+    console.log(`shitsnacks`);
+})
+
+function formReset(){
+
+}
