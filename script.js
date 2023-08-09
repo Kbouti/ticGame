3fbdb8245d02264279b0b5200178bf698c9f67be
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-09 12:09:53 -0700

    Change gameboard from px to vh units

---
+++ script.js
@@ -0,0 +1,8 @@
+let cacheDom = (function(){
+const gameboardDiv = document.getElementById(`gameboard`);
+
+})()
+
+let player = prompt(`What's your name?`);
+
+console.log(`Player's name is ${player}`);
